5e43b04805b4ce6f2ac9d0ff1f6cea029b0ed1e1
Coolblubird <80004413+Coolblubird@users.noreply.github.com>
2021-07-28 10:31:23 -0600

    update-2
    updated the ability to have multiple party member tabs open

--- Final Project/Battler.java
+++ Final Project/Battler.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import javafx.scene.image.Image;
+
 interface Battler{
 
 	void statsUp(int a, int b);
@@ -7,23 +10,35 @@ interface Battler{
 class PartyMember implements Battler{
 	String name;
 	String fileName;
+	String atk1;
+	String atk2;
+	String atk3;
 	int atk;
 	int def;
 	int mhp;
 	int chp;
 	int explr;
+	Image facePlateSpr;
+	Image overWorldSpr;
+	boolean ko = false;
 	
-	PartyMember(String newName, int newAtk, int newDef, int newExplr, int newMhp){
+	PartyMember(String newName, int newAtk, int newDef, int newExplr, int newMhp, String newATK1, String newATK2, String newATK3){
 		this.name = newName;
-		this.fileName = newName.toLowerCase();
+		this.fileName = newName.toLowerCase().trim();
 		this.atk = newAtk;
 		this.def = newDef;
 		this.explr = newExplr;
 		this.mhp = newMhp;
 		this.chp = newMhp;
+		this.facePlateSpr = new Image("/images/partyMembers/" + this.fileName + "/facePlate.PNG");
+		this.overWorldSpr = new Image("/images/partyMembers/" + this.fileName + "/overWorld.PNG");
+		this.atk1 = newATK1;
+		this.atk2 = newATK2;
+		this.atk3 = newATK3;
 	}
 		
-	double statsUp(int stat, int amount){
+	@Override
+	public void statsUp(int stat, int amount){
 		switch (stat) {
 			case 1:
 				this.chp+=amount;
@@ -37,7 +52,8 @@ class PartyMember implements Battler{
 		}	
 	}
 		
-	double statsDown(int stat, int amount){
+	@Override
+	public void statsDown(int stat, int amount){
 		switch (stat) {
 			case 1:
 				this.chp-=amount;
@@ -50,6 +66,18 @@ class PartyMember implements Battler{
 				break;
 		}	
 	}
+	
+	boolean isKO(){
+		return this.ko;
+	}
+		
+	Image getFP(){
+		return this.facePlateSpr;
+	}
+		
+	Image getOW(){
+		return this.overWorldSpr;
+	}
 }
 
 class Enemy implements Battler{
@@ -57,12 +85,50 @@ class Enemy implements Battler{
 	String fileName;
 	int atk;
 	int def;
-	int hp;
+	int chp;
+	Image btleSpr;
+	boolean ko = false;
 	
-	Enemy(String newName, int newAtk, int newDef){
+	Enemy(String newName, int newAtk, int newDef,int newCHP){
 		this.name = newName;
-		this.fileName = newName.toLowerCase();
+		this.fileName = newName.toLowerCase().trim();
 		this.atk = newAtk;
 		this.def = newDef;
+		this.chp = newCHP;
+		this.btleSpr = new Image("/images/enemies/" + this.fileName + ".PNG");
+	}
+	
+	@Override
+	public void statsUp(int stat, int amount){
+		switch (stat) {
+			case 1:
+				this.chp+=amount;
+				break;
+			case 2:
+				this.atk+=amount;
+				break;
+			case 3:
+				this.def+=amount;
+				break;
+		}	
+	}
+		
+	@Override
+	public void statsDown(int stat, int amount){
+		switch (stat) {
+			case 1:
+				this.chp-=amount;
+				break;
+			case 2:
+				this.atk-=amount;
+				break;
+			case 3:
+				this.def-=amount;
+				break;
+		}	
+	}
+	
+	boolean isKO(){
+		return ko;
 	}
 }
--- Final Project/DataMiners.java
+++ Final Project/DataMiners.java
@@ -3,17 +3,26 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.TabPane.TabClosingPolicy;
 import javafx.scene.layout.*;
+import javafx.scene.paint.*;
 import javafx.stage.Stage;
 import javafx.geometry.*;
+import javafx.scene.image.*;
 
 
 public class DataMiners extends Application {   
+	//starting characters, name, atk, def, explor, hp, dmg attack, support atk, exploration atk
+	PartyMember caulder = new PartyMember("Caulder", 3, 5, 10, 20, "Endure", "Heal", "Map");
+	PartyMember co = new PartyMember("CO", 5, 5, 5, 25, "Fire!", "Cover!", "Capture!");
+	PartyMember mars = new PartyMember("Mars", 8, 6, 3, 20, "Slash", "War Cry", "Scale");
+	PartyMember ascii = new PartyMember("ASCII", 10, 8, 1, 18, "Attack Enemy", "Use Shield", "Go There");
+	PartyMember p1 = caulder;
+	PartyMember p2 = co;
+	PartyMember p3 = mars;
+	PartyMember p4 = ascii;
+	
 	@Override // Override the start method in the Application class
-	public void start(Stage primaryStage) {   
-		TabPane tabPane = new TabPane();
-		
-		//starting characters
-		PartyMember caulder = new PartyMember();
+	public void start(Stage primaryStage) {  
+		TabPane tabPane = new TabPane();	
 		
 		MenuBar menuBar = new MenuBar();
 
@@ -42,36 +51,33 @@ public class DataMiners extends Application {
 		MenuItem menuItemP1 = new MenuItem("Party Member 1");
 		menuTabs.getItems().add(menuItemP1);
 		
-		menuItemP1.setOnAction(e -> {
-			GridPane gp2 = new GridPane();
-			
-			Button bA1 = new Button("Jump");
-			
-			bA1.setOnAction(b -> {
-				System.out.println("Youch!");
-			});
-			
-			gp2.add(bA1,0,1);
-			
-			Scene scene2 = new Scene(gp2,306,100);
-			
-			Stage newWindow = new Stage();
-			newWindow.setTitle("Tax");
-			newWindow.setScene(scene2);
-			
-			newWindow.show();
-		});
+		menuItemP1.setOnAction(e -> partyWindow(p1));
+		
+		MenuItem menuItemP2 = new MenuItem("Party Member 2");
+		menuTabs.getItems().add(menuItemP2);
+		
+		menuItemP2.setOnAction(e -> partyWindow(p2));
+		
+		MenuItem menuItemP3 = new MenuItem("Party Member 3");
+		menuTabs.getItems().add(menuItemP3);
+		
+		menuItemP3.setOnAction(e -> partyWindow(p3));
+		
+		MenuItem menuItemP4 = new MenuItem("Party Member 4");
+		menuTabs.getItems().add(menuItemP4);
+		
+		menuItemP4.setOnAction(e -> partyWindow(p4));
 		
 		VBox vbox = new VBox();
 		vbox.getChildren().addAll(menuBar);
 		
 		Scene scene = new Scene(vbox, 350, 300);  
-		primaryStage.setTitle("Meal Order!"); // Set the window title
+		primaryStage.setTitle("DataMiners"); // Set the window title
 		primaryStage.setResizable(false); // bah screw resizing!
 		primaryStage.setScene(scene); // Place the scene in the window
 		primaryStage.show(); // Display the window
 	}
-
+	
 	/**
 	 * The main method is only needed for the IDE with limited
 	 * JavaFX support. Not needed for running from the command line.
@@ -80,7 +86,50 @@ public class DataMiners extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
-	
+
+	public void partyWindow(PartyMember p){
+		
+		GridPane gp2 = new GridPane();
+		
+		Button bA1 = new Button(p.atk1);
+		
+		bA1.setOnAction(b -> {
+			System.out.println("Youch!");
+		});
+		
+		Button bA2 = new Button(p.atk2);
+		
+		bA2.setOnAction(b -> {
+			System.out.println("Oof!");
+		});
+		
+		Button bA3 = new Button(p.atk3);
+		
+		bA3.setOnAction(b -> {
+			System.out.println("Ow!");
+		});
+		
+		ImageView p1FacePlate = new ImageView(p.getFP());
+		
+		gp2.add(p1FacePlate,0,0);
+		gp2.add(bA1,1,2);
+		gp2.add(bA2,1,3);
+		gp2.add(bA3,1,4);
+		gp2.add(new Label("HP: \t" + p.chp + "/" + p.mhp),0,1);
+		gp2.add(new Label("ATK: \t" + p.atk),0,2);
+		gp2.add(new Label("DEF: \t" + p.def),0,3);
+		gp2.add(new Label("Explr: \t" + p.explr),0,4);
+		gp2.setVgap(10);
+		gp2.setBackground(new Background(new BackgroundFill(Color.ALICEBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+		gp2.setPadding(new Insets(10, 10, 10, 10));
+		
+		Scene scene2 = new Scene(gp2,230,300);
+		
+		Stage newWindow = new Stage();
+		newWindow.setTitle(p.name);
+		newWindow.setScene(scene2);
+		newWindow.setResizable(false);
+		newWindow.show();
+	}
 }
 
