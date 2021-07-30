1b873815e82e5e2163dd119f8fe82b804a565e15
Coolblubird <80004413+Coolblubird@users.noreply.github.com>
2021-07-30 10:51:39 -0600

    Update-5
    real this time

--- Final Project/DataMiners.java
+++ Final Project/DataMiners.java
@@ -9,6 +9,29 @@ import javafx.geometry.*;
 import javafx.scene.image.*;
 import java.util.*;
 
+/*
+	TO DO:
+	-INTRO:
+		-SPRITES
+		-TEXT
+		-TRANSITION TO FIRST TOWN
+	-TOWNS:
+		-SPRITES (3/6) (Viral Center, Motherboard, and the Storage device)
+		-TEXT (0/5)
+		-MAP FUNCTIONALITY 
+	-MAP:
+		-SPRITES
+		-TEXT
+		-ABILITY INTERACTION
+		-COMBAT STARTING
+	-COMBAT:
+		-SPRITES FOR ENEMIES (10/20)
+		-SPRITES FOR CHARACTERS (5/20)
+		-TEXT FOR HOW COMBAT IS GOING
+		-ABILITY INTERACTION
+	-STORY:
+		-YEAH YOU SHOULD PROBABLY WRITE THAT LMAO
+*/
 
 public class DataMiners extends Application {   
 	//starting characters, name, atk, def, explor, hp, dmg attack, support atk, exploration atk
@@ -19,10 +42,17 @@ public class DataMiners extends Application {
 	PartyMember mars = new PartyMember("Mars", 8, 6, 3, 20, "Slash", "War Cry", "Scale");
 	PartyMember ascii = new PartyMember("ASCII", 10, 2, 1, 20, "Use Sword", "Use Shield", "Go There");
 	ArrayList<Item> itemsOnPerson = new ArrayList<Item>();
-	String mode = "menu";
+	String mode = "intro";
+	String cTown = "gpc";
 	MenuBar menuBarCombat = new MenuBar();
 	GridPane town = new GridPane();
 	VBox vbox = new VBox();
+	Label textForCutscene1 = new Label("This, is a computer. ");
+	Label textForCutscene2 = new Label("It may be exactly like yours, or it may not.");
+	Label textForCutscene3 = new Label("");
+	ImageView picForCutscene = new ImageView(new Image("/images/cutscenes/intro-1.png"));
+	Button btnIntro = new Button("Next...");
+	int textIntro = 0;
 	
 	@Override // Override the start method in the Application class
 	public void start(Stage primaryStage) {  
@@ -109,12 +139,16 @@ public class DataMiners extends Application {
 		//-------------------MAP----------------------
 		
 		//-------------------INTRO SCENE--------------
+		btnIntro.setOnAction(e -> {
+			advanceText();
+			modeMachine();
+		});
 		
 		//-------------------TITLE--------------------
 		ImageView menulogo = new ImageView(new Image("/images/menulogo.png"));
 		
 		Button btnStart = new Button("Start");
-		btnStart.setOnAction(e -> modeMachine("intro"));
+		btnStart.setOnAction(e -> modeMachine());
 		
 		vbox.getChildren().addAll(menulogo,btnStart);
 		
@@ -137,20 +171,21 @@ public class DataMiners extends Application {
 		launch(args);
 	}
 	
-	public void modeMachine(String mode){
+	public void modeMachine(){
 		vbox.getChildren().clear();
 		switch (mode) {
 			case "intro":
-				
+				vbox.getChildren().addAll(picForCutscene,textForCutscene1,textForCutscene2,textForCutscene3,btnIntro);
 				break;
 			case "combat":
-				vbox.getChildren().addAll(menuBarCombat,);
+				vbox.getChildren().addAll(menuBarCombat);
 				break;
 			case "town":
 				vbox.getChildren().addAll(town);
 				break;
 		}
 	}
+	
 	
 	public void partyWindow(PartyMember p){
 		
@@ -159,7 +194,7 @@ public class DataMiners extends Application {
 		Button bA1 = new Button(p.atk1);
 		
 		bA1.setOnAction(b -> {
-			System.out.println("Youch!");
+			System.out.println(bA1.getText());
 		});
 		
 		Button bA2 = new Button(p.atk2);
@@ -195,6 +230,115 @@ public class DataMiners extends Application {
 		newWindow.setScene(scene2);
 		newWindow.setResizable(false);
 		newWindow.show();
+	}
+	
+	void registerButton(String ability){
+		switch (mode) {
+			case "combat":
+				//register attack
+				break;
+			case "town":
+				//register map movement
+				break;
+		}
+	}
+	
+	
+	public void advanceText(){
+		textIntro++;
+		switch (textIntro) {
+			case 1:
+				textForCutscene1.setText("The important part is...");
+				textForCutscene2.setText("that if this is the inside of your computer,");
+				textForCutscene3.setText("");
+				break;
+			case 2:
+				textForCutscene1.setText("How does the computer exactly work?");
+				textForCutscene2.setText("");
+				textForCutscene3.setText("");
+				break;
+			case 3:
+				textForCutscene1.setText("Is it magic?");
+				textForCutscene2.setText("Does your computer manage electricity into");
+				textForCutscene3.setText("inner components???");
+				break;
+			case 4:
+				textForCutscene1.setText("I like to believe there are li'l' creatures in there.");
+				textForCutscene2.setText("Like a tiny guy who does the GPS and stuff,");
+				textForCutscene3.setText("you know?");
+				break;
+			case 5:
+				textForCutscene1.setText("All doing the hard work for you.");
+				textForCutscene2.setText("");
+				textForCutscene3.setText("...Probably. ");
+				break;
+			case 6:
+				textForCutscene1.setText("When they are on the job, atleast.");
+				textForCutscene2.setText("Actually that would explain why my PC has");
+				textForCutscene3.setText("been running slow lately-");
+				break;
+			case 7:
+				textForCutscene1.setText("You know, another strange thought, ");
+				textForCutscene2.setText("what happens when a virus gets in?");
+				textForCutscene3.setText("How do they fight it?");
+				picForCutscene = new ImageView(new Image("/images/cutscenes/intro-2.png"));
+				break;
+			case 8:
+				textForCutscene1.setText("(Elsewhere... ");
+				textForCutscene2.setText("Inside of your computer...)");
+				textForCutscene3.setText("");
+				picForCutscene = new ImageView(new Image("/images/cutscenes/black.png"));
+				break;
+			case 9:
+				textForCutscene1.setText("Hey!");
+				textForCutscene2.setText("What's going on here?!");
+				textForCutscene3.setText("");
+				picForCutscene = new ImageView(new Image("/images/cutscenes/intro-3.png"));
+				break;
+			case 10:
+				textForCutscene1.setText("All routes to The RAMopolis district");
+				textForCutscene2.setText("have been closed due to the");
+				textForCutscene3.setText("virus emergency.");
+				picForCutscene = new ImageView(new Image("/images/cutscenes/intro-4.png"));
+				break;
+			case 11:
+				textForCutscene1.setText("W-what?");
+				textForCutscene2.setText("That's right next to my home,");
+				textForCutscene3.setText("Coolantia! How did this happen?");
+				picForCutscene = new ImageView(new Image("/images/cutscenes/intro-3.png"));
+				break;
+			case 12:
+				textForCutscene1.setText("We are looking into that,");
+				textForCutscene2.setText("sir. We apologize for the");
+				textForCutscene3.setText("inconvience this may cause.");
+				picForCutscene = new ImageView(new Image("/images/cutscenes/intro-4.png"));
+				break;
+			case 13:
+				textForCutscene1.setText("Sigh...");
+				textForCutscene2.setText("This is rough.");
+				textForCutscene3.setText("Is there anything I can do?");
+				picForCutscene = new ImageView(new Image("/images/cutscenes/intro-3.png"));
+				break;
+			case 14:
+				textForCutscene1.setText("There's a meeting going on");
+				textForCutscene2.setText("at the Graphics Processing Center.");
+				textForCutscene3.setText("Everyone is welcome.");
+				picForCutscene = new ImageView(new Image("/images/cutscenes/intro-4.png"));
+				break;
+			case 15:
+				textForCutscene1.setText("Plus I don't think we have");
+				textForCutscene2.setText("a representive from Coolantia.");
+				textForCutscene3.setText("");
+				break;
+			case 16:
+				textForCutscene1.setText("Alright, I'll be there.");
+				textForCutscene2.setText("");
+				textForCutscene3.setText("");
+				picForCutscene = new ImageView(new Image("/images/cutscenes/intro-3.png"));
+				break;
+			case 17:
+				mode = "town";
+		}
 	}
 }
 
